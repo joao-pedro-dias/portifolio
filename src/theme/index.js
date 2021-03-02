21a8197dbd94e882e3d36bec3af87920b800a011
moishawa <jpdias1999z@gmail.com>
2021-03-02 06:14:17 -0300

    GlobalStyle and Theme

---
+++ src/theme/index.js
@@ -0,0 +1,55 @@
+import { typographyVariants } from './typographyVariants';
+
+export const colors = {
+  background: {
+    light: {
+      color: '#FFFFFF',
+    },
+    main: {
+      color: '#F2F2F2',
+    },
+  },
+  borders: {
+    main: {
+      color: '#F1F1F1',
+    },
+  },
+  primary: {
+    main: {
+      color: '#6C63FF',
+      contrastText: 'white',
+    },
+  },
+  secondary: {
+    main: {
+      color: '#4b45b2',
+      contrastText: '#fff',
+    },
+  },
+  tertiary: {
+    main: {
+      color: '#070C0E',
+      contrastText: '#fff',
+    },
+    light: {
+      color: '#88989E',
+      contrastText: '#fff',
+    },
+  },
+  modes: {
+    dark: {},
+  },
+};
+
+// export const breakpoints = {
+//   xs: 0,
+//   sm: 480,
+//   md: 768,
+//   lg: 992,
+//   xl: 1200,
+// };
+
+export default {
+  colors,
+  typographyVariants,
+};
